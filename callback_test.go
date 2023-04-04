d4daf82bb3eb18d6ddfb3036c320ca584e1954c2
TotallyGamerJet <TotallyGamerJet@users.noreply.github.com>
2023-04-04 13:40:33 -0400

    purego: build tags

--- callback_test.go
+++ callback_test.go
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023 The Ebitengine Authors
 
-//go:build darwin
+//go:build darwin || linux
 
 package purego_test
 
@@ -14,7 +14,7 @@ import (
 	"github.com/ebitengine/purego"
 )
 
-// TestCallGoFromSharedLib is a test that checks for stack corruption on darwin/arm64
+// TestCallGoFromSharedLib is a test that checks for stack corruption on darwin or linux arm64
 // when C calls Go code from a non-Go thread in a dynamically loaded share library.
 func TestCallGoFromSharedLib(t *testing.T) {
 	libFileName := filepath.Join(t.TempDir(), "libcbtest.so")
